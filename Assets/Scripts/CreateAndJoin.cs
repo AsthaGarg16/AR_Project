a53ac109daf61dcaaabe8671b0fd7377be9a36ff
AsthaGarg16 <asthagarg1611@gmail.com>
2023-03-23 23:41:28 +0800

    updates to joining and rooms

---
+++ Assets/Scripts/CreateAndJoin.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class CreateAndJoin : MonoBehaviourPunCallbacks
+{
+
+    public TMP_InputField input_create;
+    public TMP_InputField input_join;
+
+    public void CreateRoom()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            if (!string.IsNullOrEmpty(input_join.text))
+            {
+                PhotonNetwork.CreateRoom(input_create.text, new RoomOptions() { IsVisible = true, IsOpen = true }) ;
+            }
+        }
+    }
+
+    public void JoinRoom()
+    {
+
+        if (PhotonNetwork.IsConnected)
+        {
+            if (!string.IsNullOrEmpty(input_join.text))
+            {
+                PhotonNetwork.JoinRoom(input_join.text);
+                Debug.Log($"{PhotonNetwork.CurrentRoom.Name} joined!");
+            }
+        }
+        
+    }
+
+    public void JoinRoomInList(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"{PhotonNetwork.CurrentRoom.Name} joined!");
+        //switch scene
+    }
+}
