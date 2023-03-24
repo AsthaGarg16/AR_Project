3e60c65808ad91a97c5322a97419fc259f9631d4
AsthaGarg16 <asthagarg1611@gmail.com>
2023-03-24 14:52:40 +0800

    username

--- Assets/Scripts/ProfileManager.cs
+++ Assets/Scripts/ProfileManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Photon.Pun;
 
 public class ProfileManager : MonoBehaviour
 {
+    public TMP_InputField usernameInput;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveProfile()
     {
-        
+        SetUsername();
+    }
+
+    public void SetUsername()
+    {
+        PhotonNetwork.NickName = usernameInput.text;
     }
 }
